47a556c2acd56c2910e7b777fb5e838bf5c91999
Chadwick <mchadwick@alteryx.com>
2018-02-08 12:58:38 -0700

    Added node-addon-api dependency

---
+++ Node.js/node-addon-api/test/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let testModules = [
+  'arraybuffer',
+  'asyncworker',
+  'buffer',
+  'error',
+  'external',
+  'function',
+  'name',
+  'object',
+  'typedarray',
+  'objectwrap'
+];
+
+if (typeof global.gc === 'function') {
+  console.log('Starting test suite\n');
+
+  // Requiring each module runs tests in the module.
+  testModules.forEach(name => {
+    console.log(`Running test '${name}'`);
+    require('./' + name);
+  });
+
+  console.log('\nAll tests passed!');
+} else {
+  // Make it easier to run with the correct (version-dependent) command-line args.
+  const child = require('./napi_child').spawnSync(process.argv[0], [ '--expose-gc', __filename ], {
+    stdio: 'inherit',
+  });
+  process.exitCode = child.status;
+}
