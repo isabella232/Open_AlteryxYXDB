616dd96c386747153f953aa18c838840d40fa5c2
piguy314159265 <dylblanchard@gmail.com>
2018-02-09 02:14:22 -0600

    r package requires flat file structure + fix compiler incompatibilities

---
+++ R/anRpackage/src/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <SDKDDKVer.h>
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+#include <Windows.h>
+#undef max
+#undef min
+
+#include <memory>
+#include <string>
+#include <algorithm>
+#include "SrcLib_Replacement.h"
+
+#define BOOST_PYTHON_STATIC_LIB
